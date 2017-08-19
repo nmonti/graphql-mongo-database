af81c445872ba010c2f38a9ab502deaf2650a2c5
nmonti <nickmax123@gmail.com>
2017-08-18 22:03:59 -0400

    webpack not working yet

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        options: {
+          presets: ['env']
+        }
+      }
+    ]
   }
 };
